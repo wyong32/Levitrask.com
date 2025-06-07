78d87d1e43e5d4389cba9a8a53cbc336a155c9df
你的GitHub用户名 <GitHub>
2025-06-07 11:30:00 +0800

    修改路由跳转404问题补充11

--- Levitrask/src/views/NotFoundView.vue
+++ Levitrask/src/views/NotFoundView.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="not-found-page">
-    <h1>404 - Page Not Found</h1>
+    <h1>410 - Page Not Found</h1>
     <p>Sorry, the page you are looking for does not exist.</p>
     <router-link to="/">Go back to Home</router-link>
   </div>
